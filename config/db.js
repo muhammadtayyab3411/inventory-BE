85bc54fafb5de30be795dec423a0454f9d0df297
Hamza <developerhamza29@gmail.com>
2023-08-02 23:31:22 +0500

    Added Notifications using sockets, and some other improvements

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
 
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 const connection = mysql.createConnection({
   host: DB_HOST,
@@ -10,7 +10,7 @@ const connection = mysql.createConnection({
 });
 
 // Name of the database to be created
-const dbName = 'koboweb';
+const dbName = "koboweb";
 
 // SQL queries to create the database and tables
 const createDatabaseQuery = `CREATE DATABASE IF NOT EXISTS ${dbName}`;
@@ -38,18 +38,55 @@ const createPasswordResetsTableQuery = `
   )
 `;
 
+const createProductsTableQuery = `
+CREATE TABLE IF NOT EXISTS ${dbName}.products (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  category TEXT,
+  buying_price INT,
+  quantity INT,
+  unit TEXT,
+  expiry_date date,
+  threshold_value INT,
+  name text,
+  user_id INT,
+  sold_amount INT
+)
+`;
+
+const createSalesTableQuery = `
+CREATE TABLE IF NOT EXISTS ${dbName}.sales (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  product_id INT,
+  quantity_sold INT,
+  sale_date date
+)
+`;
+
+const createUsersDetailTableQuery = `
+CREATE TABLE IF NOT EXISTS ${dbName}.users_detail (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  phone VARCHAR(255),
+  country VARCHAR(100),
+  language VARCHAR(255),
+  profile_picture text,
+  user_id INT
+)
+`;
+
 // Connect to the MySQL server
 connection.connect((error) => {
   if (error) {
-    console.error('Error connecting to the MySQL server:', error);
+    console.error("Error connecting to the MySQL server:", error);
     return;
   }
-  console.log('Connected to the MySQL server');
+  console.log("Connected to the MySQL server");
 
   // Create the koboweb database
   connection.query(createDatabaseQuery, (dbError) => {
     if (dbError) {
-      console.error('Error creating the database:', dbError);
+      console.error("Error creating the database:", dbError);
       connection.end();
       return;
     }
@@ -59,7 +96,7 @@ connection.connect((error) => {
     // Use the koboweb database for the subsequent queries
     connection.query(`USE ${dbName}`, (useDbError) => {
       if (useDbError) {
-        console.error('Error using the database:', useDbError);
+        console.error("Error using the database:", useDbError);
         connection.end();
         return;
       }
@@ -67,12 +104,12 @@ connection.connect((error) => {
       // Create the users table
       connection.query(createUsersTableQuery, (usersTableError) => {
         if (usersTableError) {
-          console.error('Error creating the users table:', usersTableError);
+          console.error("Error creating the users table:", usersTableError);
           connection.end();
           return;
         }
 
-        console.log('Users table created or already exists');
+        console.log("Users table created or already exists");
 
         // Create the password_resets table
         connection.query(
@@ -80,14 +117,56 @@ connection.connect((error) => {
           (passwordResetsTableError) => {
             if (passwordResetsTableError) {
               console.error(
-                'Error creating the password_resets table:',
+                "Error creating the password_resets table:",
                 passwordResetsTableError
               );
               connection.end();
               return;
             }
 
-            console.log('password_resets table created or already exists');
+            console.log("password_resets table created or already exists");
+          }
+        );
+
+        // Create the products table
+        connection.query(createProductsTableQuery, (productsTableError) => {
+          if (productsTableError) {
+            console.error(
+              "Error creating the products table:",
+              productsTableError
+            );
+            connection.end();
+            return;
+          }
+
+          console.log("products table created or already exists");
+        });
+
+        // Create the sales table
+        connection.query(createSalesTableQuery, (salesTableError) => {
+          if (salesTableError) {
+            console.error("Error creating the sales table:", salesTableError);
+            connection.end();
+            return;
+          }
+
+          console.log("sales table created or already exists");
+        });
+
+        // Create the users detials table
+        connection.query(
+          createUsersDetailTableQuery,
+          (usersDetailTableError) => {
+            if (usersDetailTableError) {
+              console.error(
+                "Error creating the users detail table:",
+                usersDetailTableError
+              );
+              connection.end();
+              return;
+            }
+
+            console.log("users detail table created or already exists");
           }
         );
       });
--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-require('dotenv').config();
-require('./config/db');
-const userRouter = require('./routes/userRoute');
-const productRouter = require('./routes/product');
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+require("./config/db");
+const userRouter = require("./routes/userRoute");
+const productRouter = require("./routes/product");
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+const http = require("http").createServer(app);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
 const port = process.env.PORT || 8000;
 
@@ -16,20 +24,38 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: '*',
+    origin: "*",
   })
 );
 app.use(cookieParser());
 
-app.use('/api', userRouter);
-app.use('/api/products', productRouter);
+app.use("/api", userRouter);
+app.use("/api/products", productRouter);
 
 app.use((err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
-  err.message = err.message || 'Internal server error';
+  err.message = err.message || "Internal server error";
   res.status(err.statusCode).json({
     message: err.message,
   });
 });
 
-app.listen(port, () => console.log(`API running at port ${port}`));
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  // Handle the order placement event
+  socket.on("placeOrder", (data) => {
+    // Process the order and save it to the database
+    console.log(data);
+    socket.emit("orderNotification", {
+      message: "Your order has been placed!",
+    });
+  });
+
+  // Handle disconnection
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+http.listen(port, () => console.log(`Listening on port ${port}`));
